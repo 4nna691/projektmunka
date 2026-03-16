b3e407cfe2741c0d1a48332589a596eacac20d29
lidikehhh <sargalidia33@gmail.com>
2026-03-16 18:52:25 +0100

    pálya méretének megadása

--- amoba_game/Program.cs
+++ amoba_game/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace amoba_game
 {
@@ -11,6 +7,45 @@ namespace amoba_game
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Amöba játék");
+			int width = 0; 
+			int height = 0;
+			while (width == 0 || height == 0)
+			{
+				Console.WriteLine("Add meg hogy mekkora legyen a pálya: (pl: 3x3)");
+				string pályaméret = Console.ReadLine();
+				if (pályaméret.Contains("x"))
+				{
+					string[] pályaméretdarab = pályaméret.Split('x');
+					width = int.Parse(pályaméretdarab[0]);
+					height = int.Parse(pályaméretdarab[1]);
+
+
+					for (int y = 0; y < height; y++)
+					{
+						string sor = "|";
+
+						for (int x = 0; x < width; x++)
+						{
+							sor += " |";
+						}
+						Console.WriteLine(sor);
+
+						string sorelvalaszto = "-";
+						for (int x = 0; x < width; x++)
+						{
+							sorelvalaszto += "--";
+						}
+						Console.WriteLine(sorelvalaszto);
+
+
+
+					}
+				}
+				else
+				{
+					Console.WriteLine("helytelen adat");
+				}
+			}
 		}
 	}
 }
